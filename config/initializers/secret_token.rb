514b9fead79bb129e87b407e27ce60d305309263
Jan Lindblom <jan.lindblom@mittmedia.se>
2013-10-04 13:15:53 +0200

    Basics.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tidis::Application.config.secret_key_base = '19dc8b310b3e50917eb58736531afc8254a4ee8251b7a85689b753dba2e006343027c1e32b62f1b0de5ac8bc3bec491215139410e1a9dfe350f4a3f9139e3735'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tidis::Application.config.session_store :cookie_store, key: '_tidis_session'
